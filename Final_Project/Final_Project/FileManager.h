bda2caa03ef4495695ba1ad0ff1c88cdf4e35179
FenishPatel0245 <patelfenish245@gmail.com>
2025-08-05 16:37:04 -0400

    add FileManager class for data I/O

--- Final_Project/Final_Project/FileManager.h
+++ Final_Project/Final_Project/FileManager.h
@@ -1,1 +1,19 @@
-#pragma once
+#ifndef FILE_MANAGER_H
+#define FILE_MANAGER_H
+
+#include <vector>
+#include <string>
+#include "BaseClassifier.h"
+
+// Responsible for loading training/test data and saving prediction results
+class FileManager
+{
+public:
+    // Loads gravity data (x, y, z + label) from a file
+    static std::vector<GravityData> readData(const std::string& filename);
+
+    // Writes prediction results (with labels) to a file
+    static void writeOutputs(const std::string& filename, const std::vector<GravityData>& results);
+};
+
+#endif // FILE_MANAGER_H
