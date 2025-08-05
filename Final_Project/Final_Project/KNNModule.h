db963675597bbc1a74df3ad44ba1125edeceb0ec
Darshitaa27 <Dpatel6206@conestogac.on.ca>
2025-08-05 16:46:35 -0400

    add KNNModule stub implementing Classifier interface

--- Final_Project/Final_Project/KNNModule.h
+++ Final_Project/Final_Project/KNNModule.h
@@ -1,1 +1,23 @@
-#pragma once
+#ifndef KNN_MODULE_H
+#define KNN_MODULE_H
+
+#include "BaseClassifier.h"
+#include <iostream>
+
+// Placeholder for a k-Nearest Neighbour classifier
+class KNNModule : public Classifier
+{
+public:
+    // Stub: training logic not implemented
+    void train(const std::vector<GravityData>& data) override
+    {
+    }
+
+    // Stub: prediction logic not implemented
+    std::string predict(double x, double y, double z) override
+    {
+        return "KNNModule not implemented yet!";
+    }
+};
+
+#endif // KNN_MODULE_H
