38f6bc174e31c4ea732cc173a5777b93d2cf3010
Pratham598 <Prao3607@conestogac.on.ca>
2025-08-05 22:09:11 -0400

    add abstract Classifier base class with GravityData struct

--- Final_Project/Final_Project/BaseClassifier.h
+++ Final_Project/Final_Project/BaseClassifier.h
@@ -1,1 +1,31 @@
-#pragma once
+
+#ifndef BASE_CLASSIFIER_H
+#define BASE_CLASSIFIER_H
+
+#include <vector>
+#include <string>
+
+// Represents one training sample with gravity vector and orientation label
+struct GravityData
+{
+    double x;
+    double y;
+    double z;
+    std::string orientation;
+};
+
+// Abstract base class for orientation classifiers
+class Classifier
+{
+public:
+    // Train the classifier with a set of labeled samples
+    virtual void train(const std::vector<GravityData>& data) = 0;
+
+    // Predict the orientation based on new gravity input
+    virtual std::string predict(double x, double y, double z) = 0;
+
+    // Virtual destructor for proper cleanup of derived classes
+    virtual ~Classifier() {}
+};
+
+#endif // BASE_CLASSIFIER_H
